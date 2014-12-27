773178add3869cc750c729a1f09c7aabb8fc2fc4
Danny Malter <http://www.linkedin.com/in/danmalter>
2014-12-26 19:22:02 -0600

    Jon Lester
    Upload

---
+++ js/lester_fast_pitches.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#lester_fast_pitches').scianimator({
+	    'images': ['images/lester_fast_pitches1.png', 'images/lester_fast_pitches2.png', 'images/lester_fast_pitches3.png', 'images/lester_fast_pitches4.png', 'images/lester_fast_pitches5.png', 'images/lester_fast_pitches6.png', 'images/lester_fast_pitches7.png', 'images/lester_fast_pitches8.png', 'images/lester_fast_pitches9.png', 'images/lester_fast_pitches10.png', 'images/lester_fast_pitches11.png', 'images/lester_fast_pitches12.png', 'images/lester_fast_pitches13.png', 'images/lester_fast_pitches14.png', 'images/lester_fast_pitches15.png', 'images/lester_fast_pitches16.png', 'images/lester_fast_pitches17.png', 'images/lester_fast_pitches18.png', 'images/lester_fast_pitches19.png', 'images/lester_fast_pitches20.png', 'images/lester_fast_pitches21.png', 'images/lester_fast_pitches22.png', 'images/lester_fast_pitches23.png', 'images/lester_fast_pitches24.png', 'images/lester_fast_pitches25.png', 'images/lester_fast_pitches26.png', 'images/lester_fast_pitches27.png', 'images/lester_fast_pitches28.png', 'images/lester_fast_pitches29.png', 'images/lester_fast_pitches30.png', 'images/lester_fast_pitches31.png', 'images/lester_fast_pitches32.png', 'images/lester_fast_pitches33.png', 'images/lester_fast_pitches34.png', 'images/lester_fast_pitches35.png', 'images/lester_fast_pitches36.png', 'images/lester_fast_pitches37.png', 'images/lester_fast_pitches38.png', 'images/lester_fast_pitches39.png', 'images/lester_fast_pitches40.png', 'images/lester_fast_pitches41.png'],
+	    'width': 480,
+	    'delay': 100,
+	    'loopMode': 'loop'
+	});
+	$('#lester_fast_pitches').scianimator('play');
+    });
+})(jQuery);
---
+++ js/lester_slow_pitches.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#lester_slow_pitches').scianimator({
+	    'images': ['images/lester_slow_pitches1.png', 'images/lester_slow_pitches2.png', 'images/lester_slow_pitches3.png', 'images/lester_slow_pitches4.png', 'images/lester_slow_pitches5.png', 'images/lester_slow_pitches6.png', 'images/lester_slow_pitches7.png', 'images/lester_slow_pitches8.png', 'images/lester_slow_pitches9.png', 'images/lester_slow_pitches10.png', 'images/lester_slow_pitches11.png', 'images/lester_slow_pitches12.png', 'images/lester_slow_pitches13.png', 'images/lester_slow_pitches14.png', 'images/lester_slow_pitches15.png', 'images/lester_slow_pitches16.png', 'images/lester_slow_pitches17.png', 'images/lester_slow_pitches18.png', 'images/lester_slow_pitches19.png', 'images/lester_slow_pitches20.png', 'images/lester_slow_pitches21.png', 'images/lester_slow_pitches22.png', 'images/lester_slow_pitches23.png', 'images/lester_slow_pitches24.png', 'images/lester_slow_pitches25.png', 'images/lester_slow_pitches26.png', 'images/lester_slow_pitches27.png', 'images/lester_slow_pitches28.png', 'images/lester_slow_pitches29.png', 'images/lester_slow_pitches30.png', 'images/lester_slow_pitches31.png', 'images/lester_slow_pitches32.png', 'images/lester_slow_pitches33.png', 'images/lester_slow_pitches34.png', 'images/lester_slow_pitches35.png', 'images/lester_slow_pitches36.png', 'images/lester_slow_pitches37.png', 'images/lester_slow_pitches38.png', 'images/lester_slow_pitches39.png', 'images/lester_slow_pitches40.png', 'images/lester_slow_pitches41.png', 'images/lester_slow_pitches42.png', 'images/lester_slow_pitches43.png', 'images/lester_slow_pitches44.png', 'images/lester_slow_pitches45.png', 'images/lester_slow_pitches46.png', 'images/lester_slow_pitches47.png'],
+	    'width': 480,
+	    'delay': 100,
+	    'loopMode': 'loop'
+	});
+	$('#lester_slow_pitches').scianimator('play');
+    });
+})(jQuery);
